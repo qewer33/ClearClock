094e867715c6e88661fc436c427fa83e7dd40e96
Hot-Tutorials <61880709+Hot-Tutorials@users.noreply.github.com>
2021-12-20 14:13:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
  #!/usr/bin/env bash
 
 PROJECT_ID="org.kde.plasma.clearclock"
-INSTALL_LOCATION="/usr/share/plasma/plasmoids/"
+INSTALL_LOCATION="/home/$USER/.local/share/plasma/plasmoids/"
 
 mkdir "${INSTALL_LOCATION}${PROJECT_ID}"
 cp -R "package/." "${INSTALL_LOCATION}${PROJECT_ID}/"
