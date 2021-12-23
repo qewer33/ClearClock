5e7db336c8d7f957f5594c79685ed38a69c7502e
qewer33 <yerdemergul@gmail.com>
2021-12-23 09:01:55 +0300

    Revamp install.sh and rename build.sh to package.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
  #!/usr/bin/env bash
 
-PROJECT_ID="org.kde.plasma.clearclock"
-INSTALL_LOCATION="/home/$USER/.local/share/plasma/plasmoids/"
+get_project_id() {
+    line=$(grep "^X-KDE-PluginInfo-Name" package/metadata.desktop)
+    IFS='='
+    read -ra array <<< "${line}"
+    projectid=${array[1]}
+    echo ${projectid}
+}
 
-mkdir "${INSTALL_LOCATION}${PROJECT_ID}"
-cp -R "package/." "${INSTALL_LOCATION}${PROJECT_ID}/"
+PROJECT_ID=$(get_project_id)
+INSTALL_LOCATION="/home/${USER}/.local/share/plasma/plasmoids/"
+
+echo "Installing ${PROJECT_ID}"
+
+if [ ! -d "${INSTALL_LOCATION}" ]; then
+    mkdir "${INSTALL_LOCATION}${PROJECT_ID}"
+else
+    echo "Skipping directory creation: directory exists"
+fi
+
+cp -R "package/." "${INSTALL_LOCATION}${PROJECT_ID}/" &&
+echo "Successfully installed ${PROJECT_ID} to ${INSTALL_LOCATION}/" ||
+echo "Installation failed"
